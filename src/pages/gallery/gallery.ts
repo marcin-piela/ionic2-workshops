8aad6af59f2160f39a36bca5915212a1e91c3562
Marcin Piela <marcin.piela@thesoftwarehouse.pl>
2016-12-12 22:54:24 +0100

    Image sanitizer

--- src/pages/gallery/gallery.ts
+++ src/pages/gallery/gallery.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {ModalController, Events} from 'ionic-angular';
 import {NewPhotoPage} from "../new-photo/new-photo";
 import {NewImageAdded} from "../../workshop/gallery/new-image-added.event";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'page-gallery',
@@ -13,12 +14,18 @@ export class GalleryPage {
   public images:Array<string> = [];
 
   /**
+   * DomSanitizer Service
+   */
+  public domSanitizer:DomSanitizer;
+
+  /**
    * @param modalCtrl
    * @param events
      */
-  constructor(modalCtrl:ModalController, events:Events) {
+  constructor(modalCtrl:ModalController, events:Events, domSanitizer:DomSanitizer) {
     this.modalCtrl = modalCtrl;
     this.events = events;
+    this.domSanitizer = domSanitizer;
 
     this.events.subscribe(NewImageAdded.NAME, (event:[NewImageAdded]) => {
       this.images.push(event[0].image);
