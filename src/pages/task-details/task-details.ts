4a8093c1ba0a89eb98bdcc035f62de970c55fc4f
Marcin Piela <marcin.piela@thesoftwarehouse.pl>
2016-12-12 13:57:56 +0100

    Fixes

--- src/pages/task-details/task-details.ts
+++ src/pages/task-details/task-details.ts
@@ -1,22 +1,22 @@
-import { Component } from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavParams} from 'ionic-angular';
 import {Task} from "../../workshop/tasks/task";
 
 @Component({
-  selector: 'page-task-details',
-  templateUrl: 'task-details.html'
+    selector: 'page-task-details',
+    templateUrl: 'task-details.html'
 })
 export class TaskDetailsPage {
 
-  private navParams:NavParams;
-  private task:Task;
+    private navParams:NavParams;
+    public task:Task;
 
-  constructor(navParams:NavParams) {
-    this.navParams = navParams;
-    this.task = navParams.get('task');
-  }
+    constructor(navParams:NavParams) {
+        this.navParams = navParams;
+        this.task = navParams.get('task');
+    }
 
-  ionViewDidLoad() {
-    console.log('Hello TaskDetailsPage Page');
-  }
+    ionViewDidLoad() {
+        console.log('Hello TaskDetailsPage Page');
+    }
 }
